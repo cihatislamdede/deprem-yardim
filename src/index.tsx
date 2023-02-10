c20a7cf452e1d1cca5f6ea9c1a9347f6246c7c3b
frost768 <droidplus99@gmail.com>
2023-02-10 14:57:25 +0300

    icons' rights

--- src/index.tsx
+++ src/index.tsx
@@ -9,5 +9,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <footer className="flex flex-row justify-between bg-secondary-black font-roboto text-slate-100">
+      <a href="https://icons8.com/icon/85968/sms">SMS icon by Icons8</a>
+      <a href="https://icons8.com/icon/87292/ringer-volume">Ringer Volume icon by Icons8</a>
+    </footer>
   </React.StrictMode>
 );
